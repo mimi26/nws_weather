2113d63c160b4f5536b3e89dd6cb835cdaa76912
Mimi Klein <lifeistoogoodtobetrue@gmail.com>
2026-08-11 12:59:00 -0400

    refactor to create weather grid hash

--- src/app/ui/weather-grid.tsx
+++ src/app/ui/weather-grid.tsx
@@ -10,40 +10,53 @@ export default async function WeatherGrid({ url }: { url: string }) {
     prop => ({ [prop]: properties[prop as keyof WeatherProperty] }),
   );
 
-  const timeArray = [...new Array(20)].map((_elem, index) => {
+  const gridData = [...new Array(20)].map((_elem, index) => {
     const timePlusIndex = new Date(new Date().setUTCMinutes(0, 0) + (index * 60 * 60 * 1000));
-    return timePlusIndex;
-  });
+    const timeObj = {};
+    const propObj = {}
+    Object.assign(timeObj, { [timePlusIndex.toLocaleTimeString()]: propObj });
+    propertiesArray.forEach(property => {
+      const propertyArray = Object.values(property)[0]?.values;
+      const propertyKey = Object.keys(property)[0];
 
-  const getPropertyByTimeAndProperty = (time: Date, property) => {
-
-    const propertyArray = Object.entries(property)[0][1]?.values;
-
-    return propertyArray?.find((dataPoint: PropertyDataPoint) => {
-      const timeMatch = dataPoint?.validTime?.match(/.*(?=\/)/);
-      return timeMatch ? Date.parse(timeMatch[0]) === Date.parse(time.toString()) : false;
+        const propDataPoint =  propertyArray?.find((dataPoint: PropertyDataPoint) => {
+          const timeMatch = dataPoint?.validTime?.match(/.*(?=\/)/);
+          return timeMatch ? Date.parse(timeMatch[0]) === Date.parse(timePlusIndex.toString()) : false;
+        });
+      
+        Object.defineProperty(propObj, propertyKey, {
+          value: propDataPoint?.value,
+          writable: true,
+          enumerable: true,
+        });
     });
-  };
+    return timeObj;
+  });
 
   return (
     <>
-      {timeArray.map(time => {
+      {gridData.map(timeData => {
+        const [ timeKey ] = Object.keys(timeData);
+        const [dataValues] = Object.values(timeData);
+
+        const dataArray = Object.keys(WeatherProperty).map(prop => ({ [prop]: dataValues[prop] }));
         return (
-          <div key={time.toString()} className={`${styles['row-item']}`}>
-            <p className={styles.dark}>{time.toLocaleTimeString()}</p>
-            {propertiesArray.map((property, index) => {
+          <div key={timeKey} className={`${styles['row-item']}`}>
+            <p className={styles.dark}>{timeKey}</p>
+
+            {dataArray.map((property, index) => {
               const backgroundClassName = index % 2 === 0 ? styles.light : styles.dark;
-
-              const rawPropertyValue = getPropertyByTimeAndProperty(time, property)?.value;
-
+              const [ propKey ] = Object.keys(property);
               return (
                 <DataPoint
-                  key={`${Object.keys(property)[0]}-${time}`}
-                  weatherProperty={Object.keys(property)[0]}
-                  rawPropertyValue={rawPropertyValue}
+                  key={`${propKey}-${timeKey}`}
+                  weatherProperty={propKey}
+                  rawPropertyValue={property[propKey]}
                   backgroundClassName={backgroundClassName}
                 />
-            )})}
+              )
+            })}
+
           </div>
         );
       })}
