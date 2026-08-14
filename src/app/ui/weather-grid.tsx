d2717c5fb576f06dddabe15e9b3c793c58bd6fee
Mimi Klein <lifeistoogoodtobetrue@gmail.com>
2026-08-13 20:50:56 -0400

    use reduceRight instead of find

--- src/app/ui/weather-grid.tsx
+++ src/app/ui/weather-grid.tsx
@@ -1,52 +1,74 @@
-import { getRawForecast } from '@/app/lib/data';
-import { PropertyDataPoint, WeatherProperty } from '@/app/lib/types';
-import DataPoint from './data-point';
-import styles from '@/app/page.module.css';
+import { getRawForecast } from "@/app/lib/data";
+import { PropertyDataPoint, WeatherProperty } from "@/app/lib/types";
+import DataPoint from "./data-point";
+import styles from "@/app/page.module.css";
 
 export default async function WeatherGrid({ url }: { url: string }) {
   const { ...properties } = await getRawForecast(url);
 
-  const propertiesArray = Object.keys(WeatherProperty).map(
-    prop => ({ [prop]: properties[prop as keyof WeatherProperty] }),
-  );
+  const propertiesArray = Object.keys(WeatherProperty).map((prop) => ({
+    [prop]: properties[prop as keyof WeatherProperty],
+  }));
 
-  const gridData = [...new Array(20)].map((_elem, index) => {
-    const timePlusIndex = new Date(new Date().setUTCMinutes(0, 0) + (index * 60 * 60 * 1000));
+  const gridData = [...new Array(20)].map((elem, index, arr) => {
+    const timePlusIndex = new Date(
+      new Date().setUTCMinutes(0, 0) + index * 60 * 60 * 1000,
+    );
     const timeObj = {};
-    const propObj = {}
+    const propObj = {};
     Object.assign(timeObj, { [timePlusIndex.toLocaleTimeString()]: propObj });
-    propertiesArray.forEach(property => {
-      const propertyArray = Object.values(property)[0]?.values;
-      const propertyKey = Object.keys(property)[0];
+    propertiesArray.forEach((property) => {
+      const [propertyValues] = Object.values(property);
+      const propertyArray = propertyValues?.values;
+      const [propertyKey] = Object.keys(property);
 
-        const propDataPoint =  propertyArray?.find((dataPoint: PropertyDataPoint) => {
+      const target = Date.parse(timePlusIndex.toString());
+
+      const propValue = propertyArray?.reduceRight(
+        (found: PropertyDataPoint, dataPoint: PropertyDataPoint) => {
+          if (found !== undefined) {
+            return found;
+          }
           const timeMatch = dataPoint?.validTime?.match(/.*(?=\/)/);
-          return timeMatch ? Date.parse(timeMatch[0]) === Date.parse(timePlusIndex.toString()) : false;
-        });
-      
-        Object.defineProperty(propObj, propertyKey, {
-          value: propDataPoint?.value,
-          writable: true,
-          enumerable: true,
-        });
+          if (!timeMatch) {
+            return undefined;
+          }
+          const currentTimeStamp = Date.parse(timeMatch[0]);
+          // match exact hourly steps within the allowed lookback window
+          if (currentTimeStamp <= target) {
+            return dataPoint?.value;
+          }
+          return undefined;
+        },
+        undefined,
+      );
+
+      Object.defineProperty(propObj, propertyKey, {
+        value: propValue,
+        writable: true,
+        enumerable: true,
+      });
     });
     return timeObj;
   });
 
   return (
     <>
-      {gridData.map(timeData => {
-        const [ timeKey ] = Object.keys(timeData);
+      {gridData.map((timeData, index) => {
+        const [timeKey] = Object.keys(timeData);
         const [dataValues] = Object.values(timeData);
 
-        const dataArray = Object.keys(WeatherProperty).map(prop => ({ [prop]: dataValues[prop] }));
+        const dataArray = Object.keys(WeatherProperty).map((prop) => ({
+          [prop]: dataValues[prop],
+        }));
         return (
-          <div key={timeKey} className={`${styles['row-item']}`}>
+          <div key={timeKey} className={`${styles["row-item"]}`}>
             <p className={styles.dark}>{timeKey}</p>
 
             {dataArray.map((property, index) => {
-              const backgroundClassName = index % 2 === 0 ? styles.light : styles.dark;
-              const [ propKey ] = Object.keys(property);
+              const backgroundClassName =
+                index % 2 === 0 ? styles.light : styles.dark;
+              const [propKey] = Object.keys(property);
               return (
                 <DataPoint
                   key={`${propKey}-${timeKey}`}
@@ -54,9 +76,8 @@ export default async function WeatherGrid({ url }: { url: string }) {
                   rawPropertyValue={property[propKey]}
                   backgroundClassName={backgroundClassName}
                 />
-              )
+              );
             })}
-
           </div>
         );
       })}
